2036598ae71840ceab86d650ea7887a18d08be0c
Mujahid845455 <pweducationwallah@gmail.com>
2026-01-11 12:34:44 +0530

    chore: add gitignore for backend10

--- server.js
+++ server.js
@@ -49,11 +49,11 @@ const userSchema = new mongoose.Schema({
   userType: String
 }, { timestamps: true });
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
+
 
 const User = mongoose.model("User", userSchema);
 
